4d6debce58da5ec347686ba43024043f3df8537f
RafaelOliveira013 <rafaeletec013@gmail.com>
2023-06-16 13:17:25 -0300

    01:03 - Adicionando cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./*html"],
   theme: {
     extend: {
-  },
+      colors: {
+      black: '#4A4A4A',
+      vermelho: '#fa8282',
+      blue: '#0b287d',
+      green: '#37ff1c'
+    },},
   },
   plugins: [],
 }
