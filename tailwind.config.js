78e7cb669e6e966747c14dc6656a8286cfd10caa
RafaelOliveira013 <rafaeletec013@gmail.com>
2023-06-16 13:22:35 -0300

    01:04 - Adicionando espaçamentos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,42 @@
 module.exports = {
   content: ["./*html"],
   theme: {
+    
+    spacing: {
+      '32': '32rem'
+    },
+
     extend: {
       colors: {
       black: '#4A4A4A',
       vermelho: '#fa8282',
       blue: '#0b287d',
       green: '#37ff1c'
-    },},
+    },
+    width: {
+      '50': '50px'
+    },
+
+    minWidth: {
+      '1/2': '50%'
+    },
+
+    maxWidth: {
+      '70': '70%'
+    },
+
+    height: {
+      '50': '50px'
+    },
+
+    minHeight: {
+      '1/2': '50%'
+    },
+
+    maxHeight: {
+      '70': '70%'
+    },
+  },
   },
   plugins: [],
 }
